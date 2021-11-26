870224d2d66214c3bdf3f0c246cc8661c2f09261
2021-DEV1-118 <95078679+2021-DEV1-118@users.noreply.github.com>
2021-11-26 13:51:05 +0100

    Add Grid test

--- src/components/Grid/Grid.test.tsx
+++ src/components/Grid/Grid.test.tsx
@@ -1,11 +1,9 @@
-import { render, screen } from "@testing-library/react";
-
 import { Grid } from "./Grid";
+import { render } from "@testing-library/react";
 
 describe("Grid component", () => {
-  test("should render", () => {
-    render(<Grid />);
-    const text = screen.getByText(/Grid/i);
-    expect(text).toBeInTheDocument();
+  test("should render 3 rows to display the game board", () => {
+    const { container } = render(<Grid />);
+    expect(container.getElementsByClassName("grid__row").length).toBe(3);
   });
 });
