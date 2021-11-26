2152641968d3847b85fb7aec05fe613cd7221cfa
2021-DEV1-118 <95078679+2021-DEV1-118@users.noreply.github.com>
2021-11-26 13:55:04 +0100

    Green test Grid component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,3 +1,24 @@
+import { Cell } from "../Cell/Cell";
+import styles from "./Grid.module.scss";
+
 export const Grid = () => {
-  return <div>Grid</div>;
+  return (
+    <div className={styles["grid__container"]}>
+      <div className={styles["grid__row"]}>
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
+      <div className={styles["grid__row"]}>
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
+      <div className={styles["grid__row"]}>
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
+    </div>
+  );
 };
